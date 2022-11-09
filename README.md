9dbccf21bfb1e53ff09fd3d1e67626fad2f29d17
Mike Cobbett <77053+techcobweb@users.noreply.github.com>
2022-11-09 18:08:14 +0000

    Building locally script. Not because it's hard, but to be consistent with the other projects which do things in the same way

---
+++ README.md
@@ -0,0 +1,6 @@
+# Build Utils
+
+This repository is built into tools which themselves are used during a Galasa build.
+
+## To build locally
+Use the `./build-locally.sh --help` to get a description of the required parameters and environment variables.
---
+++ build-locally.sh
@@ -0,0 +1,141 @@
+#! /usr/bin/env bash 
+
+#-----------------------------------------------------------------------------------------                   
+#
+# Objectives: Build this repository code locally.
+# 
+#-----------------------------------------------------------------------------------------                   
+
+# Where is this script executing from ?
+BASEDIR=$(dirname "$0");pushd $BASEDIR 2>&1 >> /dev/null ;BASEDIR=$(pwd);popd 2>&1 >> /dev/null
+# echo "Running from directory ${BASEDIR}"
+export ORIGINAL_DIR=$(pwd)
+# cd "${BASEDIR}"
+
+cd "${BASEDIR}/.."
+WORKSPACE_DIR=$(pwd)
+
+
+#-----------------------------------------------------------------------------------------                   
+#
+# Set Colors
+#
+#-----------------------------------------------------------------------------------------                   
+bold=$(tput bold)
+underline=$(tput sgr 0 1)
+reset=$(tput sgr0)
+red=$(tput setaf 1)
+green=$(tput setaf 76)
+white=$(tput setaf 7)
+tan=$(tput setaf 202)
+blue=$(tput setaf 25)
+
+#-----------------------------------------------------------------------------------------                   
+#
+# Headers and Logging
+#
+#-----------------------------------------------------------------------------------------                   
+underline() { printf "${underline}${bold}%s${reset}\n" "$@"
+}
+h1() { printf "\n${underline}${bold}${blue}%s${reset}\n" "$@"
+}
+h2() { printf "\n${underline}${bold}${white}%s${reset}\n" "$@"
+}
+debug() { printf "${white}[.] %s${reset}\n" "$@"
+}
+info()  { printf "${white}[➜] %s${reset}\n" "$@"
+}
+success() { printf "${white}[${green}✔${white}] ${green}%s${reset}\n" "$@"
+}
+error() { printf "${white}[${red}✖${white}] ${red}%s${reset}\n" "$@"
+}
+warn() { printf "${white}[${tan}➜${white}] ${tan}%s${reset}\n" "$@"
+}
+bold() { printf "${bold}%s${reset}\n" "$@"
+}
+note() { printf "\n${underline}${bold}${blue}Note:${reset} ${blue}%s${reset}\n" "$@"
+}
+
+#-----------------------------------------------------------------------------------------                   
+# Functions
+#-----------------------------------------------------------------------------------------                   
+function usage {
+    info "Syntax: build-locally.sh [OPTIONS]"
+    cat << EOF
+Options are:
+-h | --help : Display this help text
+
+Environment Variables:
+     
+LOGS_DIR :
+    Controls where logs are placed. 
+    Optional. Defaults to creating a new temporary folder
+
+EOF
+}
+
+#-----------------------------------------------------------------------------------------                   
+# Process parameters
+#-----------------------------------------------------------------------------------------                   
+exportbuild_type=""
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     error "Unexpected argument $1"
+                                usage
+                                exit 1
+    esac
+    shift
+done
+
+#-----------------------------------------------------------------------------------------                   
+# Main logic.
+#-----------------------------------------------------------------------------------------                   
+
+source_dir="."
+
+project=$(basename ${BASEDIR})
+h1 "Building ${project}"
+
+# Create a temporary dir.
+# Note: This bash 'spell' works in OSX and Linux.
+if [[ -z ${LOGS_DIR} ]]; then
+    export LOGS_DIR=$(mktemp -d 2>/dev/null || mktemp -d -t "galasa-logs")
+    info "Logs are stored in the ${LOGS_DIR} folder."
+    info "Over-ride this setting using the LOGS_DIR environment variable."
+else
+    mkdir -p ${LOGS_DIR} 2>&1 > /dev/null # Don't show output. We don't care if it already existed.
+    info "Logs are stored in the ${LOGS_DIR} folder."
+    info "Over-ridden by caller using the LOGS_DIR variable."
+fi
+
+info "Using source code at ${source_dir}"
+cd ${BASEDIR}/${source_dir}
+
+log_file=${LOGS_DIR}/${project}.txt
+info "Log will be placed at ${log_file}"
+date > ${log_file}
+
+
+
+cat << EOF >> ${log_file}
+Using command:
+
+make all 2>&1 >> ${log_file}
+
+EOF
+
+cd ${WORKSPACE_DIR}/${project}
+
+make all 2>&1 >> ${log_file}
+
+rc=$? 
+if [[ "${rc}" != "0" ]]; then 
+    error "Failed to build the ${project}" 
+    exit 1 
+fi
+success "${project} built ok - log is at ${log_file}"
+
