52bca2188af6aa37ac7305d02df0c225b9d98dc3
Jade Carino <65225359+jadecarino@users.noreply.github.com>
2024-01-10 12:19:10 +0000

    Update slack post bot to include branch detail if provided (#36)
    Signed-off-by: Jade Carino <carino_jade@yahoo.co.uk>

--- pkg/cmd/slackpostBuilds.go
+++ pkg/cmd/slackpostBuilds.go
@@ -24,11 +24,13 @@ var (
 	}
 	pipeline        string
 	pipelineRunName string
+	branch          string
 )
 
 func init() {
 	slackpostBuildsCmd.PersistentFlags().StringVar(&pipeline, "pipeline", "", "The name of the Pipeline that failed")
 	slackpostBuildsCmd.PersistentFlags().StringVar(&pipelineRunName, "prun", "", "The name of the PipelineRun that failed")
+	slackpostBuildsCmd.PersistentFlags().StringVar(&branch, "branch", "", "The name of the branch that was being built")
 
 	slackpostBuildsCmd.MarkPersistentFlagRequired("pipeline")
 	slackpostBuildsCmd.MarkPersistentFlagRequired("prun")
@@ -40,7 +42,11 @@ func slackpostBuildsExecute(cmd *cobra.Command, args []string) {
 	fmt.Printf("Galasa Build - Slack Failed Build Report - version %v\n", rootCmd.Version)
 
 	linkToPipelineRun := fmt.Sprintf("http://localhost:8001/api/v1/namespaces/tekton-pipelines/services/tekton-dashboard:http/proxy/#/namespaces/galasa-build/pipelineruns/%s", pipelineRunName)
-	content := fmt.Sprintf("Galasa build pipeline failure:\n\n'%s' pipeline failed.\n\nPlease see %s for details.", pipeline, linkToPipelineRun)
+	branchString := ""
+	if branch != "" {
+		branchString = fmt.Sprintf(" when building the '%s' branch", branch)
+	}
+	content := fmt.Sprintf("Galasa build pipeline failure:\n\n'%s' pipeline failed%s. Please see %s for details.", pipeline, branchString, linkToPipelineRun)
 
 	client := http.Client{
 		Timeout: time.Second * 30,
